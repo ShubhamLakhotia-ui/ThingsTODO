6691b2c9ceeead5cf412446735075ce47b06bd82
ShubhamLakhotia-ui <lakhotia.sh@northeastern.edu>
2024-04-15 19:52:14 -0400

    Landing page fixes

---
+++ thingstodo/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Landing from './Landing/landing';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+function App() {
+  return (
+    <div>
+      <Landing/>
+    </div>
+  );
+}
+
+export default App;
